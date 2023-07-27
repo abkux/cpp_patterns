1b08a151e4fce98950b7613eaefcaebc49f63e4a
Abhishek <68020586+abkux@users.noreply.github.com>
2023-07-27 15:40:49 +0530

    Create p1.cpp

---
+++ p1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// Pattern #1
+/*
+ * * * *
+ * * * *
+ * * * *
+ * * * *
+*/
+
+int main() {
+    int n;
+    std::cin >> n;
+    int row = 1;
+    while(row<=n){
+        int col = 1;
+        while (col <= n) {
+            std::cout << "*" << " ";
+            col++;
+        }
+        std::cout << std::endl;
+        row++;
+    }
+    return 0;
+}
